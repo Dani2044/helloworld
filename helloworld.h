c037e1c875d50350cc7244239ad641bd38fe49bc
Dani2044 <castro-df@javeriana.edu.co>
2025-01-08 14:19:17 -0500

    Initial commit

---
+++ helloworld.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/**
+ * @brief Calculate the length of the message
+ */
+int calculateLength(std::vector<std::string> msg) {
+    return msg.size();
+}
